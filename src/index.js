8fadfd6961a581b8834b809475a517f3700e2157
gigorko1993 <gigorko1993@gmail.com>
2021-06-04 15:14:37 +0300

    make fetching from api and basic rendering of image card

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import './sass/main.scss';
+import getRefs from '../src/js/markup.js';
+import API from '../src/js/apiService.js';
+import imageCardsTpl from '../src/templates/imageCard.hbs';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const debounce = require('lodash.debounce');
+const refs = getRefs();
+
+refs.input.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const inputedText = e.target.value.trim();
+  console.log(inputedText)
+
+  API.fetchImages(inputedText).then(renderImageCard);
+}
+
+function renderImageCard(image) {
+  console.dir(image)
+  refs.cardContainer.innerHTML = '';
+  // if (country.length === 1) {
+  //   const markup = countryCardTpl(country);
+  //   refs.cardContainer.innerHTML = markup;
+  // } else if (country.length > 1 && country.length < 11) {
+  //   const markup = countriesCardsTpl(country);
+  //   refs.cardContainer.innerHTML = markup;
+  // } else if (country.length > 10) {
+  if (image.hits.length > 0) {
+      const markup = imageCardsTpl(image);
+    refs.cardContainer.innerHTML = markup;
+  } else {
+ error({
+      text: 'Too many matches found. Please enter a more specific query!',
+      delay: 3500,
+    });
+  }
+   
+   return;
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -5,13 +5,13 @@ import '@pnotify/core/dist/BrightTheme.css';
 const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&';
 const ApiKey = '9062055-da883187fb30391728e11f5fd';
 
-function fetchImages(name, pgNumber) {
-  const url = `${BASE_URL}q=${name}&page=${pgNumber}&per_page=12&key=${ApiKey}`;
+function fetchImages(name) {
+  const url = `${BASE_URL}q=${name}&page=1&per_page=12&key=${ApiKey}`;
   return fetch(url).then(res => {
     if (!res.ok) {
       res.status === 404
         ? error({
-            text: 'Sorry, we can`t find entered name of country. Please try to change inputed name.',
+            text: 'Sorry, we can`t find entered image. Please try to change inputed value.',
             delay: 3500,
           })
         : error({
